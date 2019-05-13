2496d57ec761e223440e8f90e54df56e9a1fcb31
jigar yadav <3BrainTechServices@gmail.com>
2019-05-13 17:58:45 +0530

    hot fix in check status api

--- auth/api/controller/checkStatus.js
+++ auth/api/controller/checkStatus.js
@@ -3,33 +3,27 @@ const User = require('../models/user');
 const checkStatus = (req, res) =>{
     let userData = req.validUserData;
     User.findOne({_id: userData._id},{password:0, __v:0}).exec().then((data)=>{
-        if(data){
-            if(data.active){
-                if(data.emailVerify){
-                    if(data.username){
-                        res.status(200).json({
-                            status: 200,
-                            message: 'profile fetch successfully',
-                            data
-                        })
-                    }else{
-                        error(res, 'please set your username first', {
-                            username: false
-                        })
-                    }
-                }else{
-                    error(res, 'please verify your email', {
-                        emailVerify: false
-                    })
-                }
-            }else{
-                error(res, 'account deactivated', {
-                    active: false
-                })
-            }
-        }else{
+        if(!data){
             error(res, 'user not found', {
                 user: false
+            })
+        }else if(!data.active){
+            error(res, 'account deactivated', {
+                active: false
+            })
+        }else if(!data.emailVerify){
+            error(res, 'please verify your email', {
+                emailVerify: false
+            })
+        }else if(!data.username){
+            error(res, 'please set your username first', {
+                username: false
+            })
+        }else{
+            res.status(200).json({
+                status: 200,
+                message: 'profile fetch successfully',
+                data
             })
         }
     })
