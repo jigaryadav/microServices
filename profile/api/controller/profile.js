521076513f7a5c4ceab90dc4599983eab9985f98
jigar yadav <3BrainTechServices@gmail.com>
2019-05-16 19:13:38 +0530

    hot fix

--- profile/api/controller/profile.js
+++ profile/api/controller/profile.js
@@ -1,13 +1,24 @@
 const User = require('../models/user');
+const UserFollowing = require('../models/userFollowings');
+
 
 const profile = async (req, res) => {
     let userData  = req.validUserData;
-    User.findOne({_id:userData._id}, {password:0, __v:0}).exec().then((user)=>{
+    User.findOne({_id:userData._id}, {password:0, __v:0}).lean().exec().then(async (user)=>{
         if(user){
+            let followerCount = 0 
+            let followingCount = 0
+            followingCount = await UserFollowing.find({ follower: userData._id }).countDocuments().exec();
+            followerCount = await UserFollowing.find({ following: userData._id }).countDocuments().exec();
+
             res.status(200).json({
                 status: 200, 
                 message: 'profile fetch success',
-                data: user
+                data: {
+                    ...user,
+                    followerCount,
+                    followingCount
+                }
             });
         }else{
             notFound(res)
